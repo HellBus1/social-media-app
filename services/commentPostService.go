91d94d44180d0f64a0a604868a7b7850b50e8ee8
M Ryan M <119112916+martjellino@users.noreply.github.com>
2024-03-24 16:47:13 +0700

    fix: fix issue in comment (#10)

--- services/commentPostService.go
+++ services/commentPostService.go
@@ -2,7 +2,8 @@ package services
 
 import (
 	"context"
-	"log"
+	"errors"
+	// "log"
 	"social-media-app/models/comment"
 	"time"
 
@@ -10,42 +11,50 @@ import (
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
+var ErrNotFriends = errors.New("commentator and post owner are not friends")
+var ErrPostNotFound = errors.New("post not found")
+
 func CreateCommentService(DB *pgxpool.Pool, Request comment.CommentRequest, commentatorId int) error {
 	ctx := context.Background()
 
-	tx, err := DB.BeginTx(ctx, pgx.TxOptions{})
+	tx, err := DB.Begin(ctx)
 	if err != nil {
-		log.Println("Failed to begin transaction:", err)
+		return err
+	}
+	defer tx.Rollback(ctx)
+
+	// Retrieve the post owner's ID
+	var postOwnerId int
+	err = tx.QueryRow(ctx, "SELECT user_id FROM Posts WHERE id = $1", Request.PostID).Scan(&postOwnerId)
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			return ErrPostNotFound
+		}
 		return err
 	}
 
-	defer func() {
-		if p := recover(); p != nil {
-			if rollbackErr := tx.Rollback(ctx); rollbackErr != nil {
-				log.Println("Failed to rollback transaction:", rollbackErr)
-			}
-			panic(p)
-		} else if err != nil {
-			if rollbackErr := tx.Rollback(ctx); rollbackErr != nil {
-				log.Println("Failed to rollback transaction:", rollbackErr)
-			}
-			log.Println("Transaction aborted:", err)
-		}
-	}()
+	// Check if the commentator and post owner are friends
+	var isFriend bool
+	err = tx.QueryRow(ctx, "SELECT EXISTS(SELECT 1 FROM Friendship WHERE (user_id = $1 AND friend_id = $2) OR (user_id = $2 AND friend_id = $1))", commentatorId, postOwnerId).Scan(&isFriend)
+	if err != nil {
+		return err
+	}
 
-	// var id int
+	if !isFriend {
+		return ErrNotFriends
+	}
 
-	// Prepare statement within the transaction
+	// Insert comment into database
 	_, err = tx.Exec(ctx, "INSERT INTO comments (commentator_id, post_id, comment, created_at, updated_at) VALUES ($1, $2, $3, $4, $5)", commentatorId, Request.PostID, Request.Comment, time.Now(), time.Now())
 	if err != nil {
-		log.Println("Error executing insert statement:", err)
 		return err
 	}
 
 	// Commit the transaction
-	if err = tx.Commit(ctx); err != nil {
-		log.Println("Failed to commit transaction:", err)
+	err = tx.Commit(ctx)
+	if err != nil {
 		return err
 	}
+
 	return nil
 }
